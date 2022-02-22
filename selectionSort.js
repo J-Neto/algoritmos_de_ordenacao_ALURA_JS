f93d90659f90d627a46c2bc6c97079325abe3492
José <ribeirojoseph44@gmail.com>
2022-02-22 08:47:29 -0300

    Criacao do 'selectionSort.js' - v1.0.0'

---
+++ selectionSort.js
@@ -0,0 +1,25 @@
+// FUNÇÃO PRINCIPAL
+
+// importando lista de livros e função interna
+const livros = require('./listaLivros');
+const menorValor = require('./menorValor');
+
+console.log('Lista inicial: ', livros);
+
+// Função principal
+for (let atual = 0; atual < livros.length; atual++) {
+
+    // Índice do menor valor
+    let menor = menorValor(livros, atual);
+
+    // Salva o valor do livro atual e do livro de menor preço
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
+
+    // Troca o livro atual e o de menor preço de lugar
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
+}
+
+// Exibe a lista ordenada
+console.log('\nLista ordenada: ', livros);
